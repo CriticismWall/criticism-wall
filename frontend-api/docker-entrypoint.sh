317fae1fc978b74484fc72c9857dc6bd1894414e
ywb <yuweibin@boostfield.com>
2020-11-29 14:25:56 +0800

    Fix docker-compose

--- frontend-api/docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-if [ -z "$SB_ARGS" ]; then
-    echo "running with default spring config"
-    java -Djava.security.egd="file:/dev/./urandom" ${JAVA_OPTS} -jar /app.jar
-else
-    echo "running with configration ${SB_ARGS}"
-    java -Djava.security.egd="file:/dev/./urandom" ${JAVA_OPTS} ${SB_ARGS} -jar /app.jar
-fi
